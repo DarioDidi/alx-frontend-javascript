0beb1eedd5160513c11a9a375ef60db0eea6cf0c
Dario Didi <dididario.dd2@gmail.com>
2025-05-25 07:37:48 +0300

    add task_5

---
+++ 0x04-TypeScript/task_5/main.ts
@@ -0,0 +1,32 @@
+interface MajorCredits {
+	credits: number,
+	_majorBrand: 'major';
+}
+interface MinorCredits {
+	credits: number,
+	_minorBrand: 'minor';
+}
+
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+	return {
+		credits: subject1.credits + subject2.credits,
+		_majorBrand: 'major'
+	} as MajorCredits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+	return {
+		credits: subject1.credits + subject2.credits,
+		_minorBrand: 'minor'
+	} as MinorCredits;
+}
+
+
+const major1: MajorCredits = { credits: 3, _majorBrand: 'major' };
+const major2: MajorCredits = { credits: 4, _majorBrand: 'major' };
+const minor1: MinorCredits = { credits: 2, _minorBrand: 'minor' };
+const minor2: MinorCredits = { credits: 1, _minorBrand: 'minor' };
+
+console.log(sumMajorCredits(major1, major2));  // { credits: 7, _majorBrand: 'major' }
+console.log(sumMinorCredits(minor1, minor2));
